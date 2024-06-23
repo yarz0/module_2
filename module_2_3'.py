504631abc143e40b1748c9e7f909d1f7192b1e17
yarz0 <40396015+yarz0@users.noreply.github.com>
2024-06-23 12:54:30 +1000

    Add files via upload

---
+++ module_2_3'.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while True:
+    while index < len(my_list):
+        if my_list[index] > 0:
+            print(my_list[index])
+        index = index + 1
+    break
+
+
